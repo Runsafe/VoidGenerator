4c037bd9d26529d43e46643682856b9cfeef1dae
Morten Nilsen <morten@runsafe.no>
2013-05-25 01:23:23 +0200

    Make somewhere to land

--- src/no/runsafe/voidgenerator/Generator.java
+++ src/no/runsafe/voidgenerator/Generator.java
@@ -16,6 +16,10 @@ public class Generator extends ChunkGenerator
 		byte[][] chunk = new byte[8][4096];
 		for (int n = 0; n < 8; ++n)
 			Arrays.fill(chunk[n], air);
+
+		if (cx == 0 && cz == 0)
+			chunk[4][0] = (byte) Material.STONE.getId();
+
 		return chunk;
 	}
 }
--- src/no/runsafe/voidgenerator/Plugin.java
+++ src/no/runsafe/voidgenerator/Plugin.java
@@ -1,7 +1,11 @@
 package no.runsafe.voidgenerator;
 
 import no.runsafe.framework.RunsafePlugin;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.generator.ChunkGenerator;
+
+import java.util.Random;
 
 public class Plugin extends RunsafePlugin
 {
@@ -13,5 +17,10 @@ public class Plugin extends RunsafePlugin
 	public ChunkGenerator getDefaultWorldGenerator(String worldName, String id)
 	{
 		return new Generator();
+	}
+
+	public Location getFixedSpawnLocation(World world, Random random)
+	{
+		return new Location(world, 0.5D, 64D, 0.5D);
 	}
 }
